1d116344f8c8c27559d292700c4e8b7f146d5133
KaloyanYosifov <kaloqn665@gmail.com>
2022-05-05 18:01:44 +0200

    Add initial version of adding todos

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,19 @@
 module Main where
+import           Text.Printf (printf)
+
+mapWithIndex :: (Int -> a -> b) -> [a] -> [b]
+mapWithIndex = mapWithIndex' 0
+    where mapWithIndex' _ _ []     = []
+          mapWithIndex' i f (x:xs) = f i x:mapWithIndex' (i + 1) f xs
+
+tasks :: String -> String
+tasks = unlines . mapWithIndex (printf "[%i] %s") . lines
 
 main :: IO ()
-main = putStrLn "test"
+main = do
+    text <- readFile "./todos.txt"
+    putStrLn $ tasks text
+    putStrLn "Add new task"
+    task <- getLine
+    writeFile "./todos.txt" $ text ++ task ++ "\n"
+    return ()

992a29a4f86626cfd726beda984a8c77d63a6024
KaloyanYosifov <kaloqn665@gmail.com>
2022-05-06 09:39:56 +0200

    Integrate delete task flow

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 import           Control.Monad (unless)
+import           Data.List     (delete)
 import           Text.Printf   (printf)
+import           Text.Read     (readMaybe)
 
 mapWithIndex :: (Int -> a -> b) -> [a] -> [b]
 mapWithIndex = mapWithIndex' 0
@@ -9,6 +11,28 @@ mapWithIndex = mapWithIndex' 0
 
 tasks :: String -> String
 tasks = unlines . mapWithIndex (printf "[%i] %s") . lines
+
+deleteTaskFlow :: IO ()
+deleteTaskFlow = do
+    text <- readFile "./todos.txt"
+    putStrLn $ tasks text
+    putStrLn "Pick a task to delete"
+    taskId <- getLine
+    case readMaybe taskId :: Maybe Int of
+      Just x -> do
+            let tasksList = lines text
+            if x < 0 || x >= length tasksList
+                then
+                    do
+                    putStrLn "Invalid task Id! Press enter to continue"
+                    getChar
+                    return ()
+                else
+                    writeFile "./todos.txt" $ unlines $ delete (tasksList !! x) tasksList
+
+            main
+
+      Nothing -> main
 
 addTaskFlow :: IO ()
 addTaskFlow = do
@@ -24,8 +48,10 @@ main = do
     putStrLn "Pick commands"
     putStrLn "- Type \"exit\" to exit the program"
     putStrLn "- Type \"add\" to add a new task"
+    putStrLn "- Type \"delete\" to add a new task"
     command <- getLine
     case command of
-      "exit" -> return ()
-      "add"  -> addTaskFlow
-      _      -> main
+      "exit"   -> return ()
+      "add"    -> addTaskFlow
+      "delete" -> deleteTaskFlow
+      _        -> main
